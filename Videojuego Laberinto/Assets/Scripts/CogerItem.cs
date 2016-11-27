86d5c8183812925b66cd206ca46c6ac50bb2b8ef
Alexis Daniel Fuentes Pérez <alex.fp95@icloud.com>
2016-11-27 20:24:53 +0000

    Implemented scripts for VR
    You can take items from the maze

---
+++ Videojuego Laberinto/Assets/Scripts/CogerItem.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CogerItem : MonoBehaviour {
+
+	private GameObject scriptsContenedor;
+	private PlayerBD jugadorBD;
+
+	void Awake () {
+		scriptsContenedor = GameObject.FindGameObjectWithTag ("Scripts");
+		jugadorBD = scriptsContenedor.GetComponent<PlayerBD> ();
+	}
+
+	public void cogerItem () {
+		for (int i = 0; i < jugadorBD.slots.Length; i++) {
+			if (jugadorBD.slots [i] == "") {
+				jugadorBD.slots [i] = this.tag;
+				Debug.Log ("Cogio: " + jugadorBD.slots [i]);
+				Destroy (this.gameObject);
+			}
+		}
+	}
+}
